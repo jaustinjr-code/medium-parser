903fb4158defbac7f70ac8ff072cd3150d6c4cff
jaustinjr.code <jaustinjr.collab@gmail.com>
2026-02-14 19:29:42 -0800

    Enable Default and Spread Package Imports

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    export { fetcher };
+    export { rssFeedHelper };
+    export { errors };
+}
+export default _default;
+import * as fetcher from "./fetcher";
+import * as rssFeedHelper from "./rssFeedHelper";
+import * as errors from "./errors";
+export { fetcher, rssFeedHelper, errors };
---
+++ index.js
@@ -0,0 +1,6 @@
+import * as fetcher from "./fetcher";
+import * as rssFeedHelper from "./rssFeedHelper";
+import * as errors from "./errors";
+
+export { fetcher, rssFeedHelper, errors };
+export default { fetcher, rssFeedHelper, errors };
