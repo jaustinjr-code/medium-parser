6c3395d6464f8cf72f5fbffe4d6a9c10d46aaece
jaustinjr.code <jaustinjr.collab@gmail.com>
2026-02-22 10:58:03 -0800

    Use TSC Default Declaration Generation

--- errors.d.ts
+++ errors.d.ts
@@ -3,13 +3,22 @@ export class RssError extends Error {
   options: any;
   cause: any;
 }
+
 export class StructureError extends Error {
   constructor(message: string, options: any);
   options: any;
   cause: any;
 }
+
 export class UnknownAuthorError extends Error {
   constructor(message: string, options: any);
   options: any;
   cause: any;
 }
+
+declare namespace _default {
+  export { RssError };
+  export { StructureError };
+  export { UnknownAuthorError };
+}
+export default _default;
--- rssFeedHelper.d.ts
+++ rssFeedHelper.d.ts
@@ -1,1 +1,6 @@
 export function fetchRssFeed(feedUrl: any): Promise<any>;
+
+declare namespace _default {
+  export { fetchRssFeed };
+}
+export default _default;
