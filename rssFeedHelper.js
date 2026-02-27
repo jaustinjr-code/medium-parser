ecadb318eadb6c800b77e26473cbd27357412151
jaustinjr.code <jaustinjr.collab@gmail.com>
2026-02-26 21:26:53 -0800

    Remove RSS Feed Helper
    The helper function does not need to be visible. Includes JSDoc for fetchRssFeed.

--- rssFeedHelper.js
+++ rssFeedHelper.js
@@ -1,5 +1,13 @@
 import { NetworkError, HttpError, ParseError } from "./errors";
 
+/**
+ * Fetches an RSS feed from the given URL.
+ * @param {string} feedUrl - The URL of the RSS feed to fetch
+ * @returns {Promise<Object>} The parsed JSON response from the feed
+ * @throws {NetworkError} If the network request fails
+ * @throws {HttpError} If the feed server returns an error status
+ * @throws {ParseError} If the response cannot be parsed as JSON
+ */
 export const fetchRssFeed = async (feedUrl) => {
   const res = await fetch(
     `https://api.allorigins.win/get?url=${feedUrl}`,
