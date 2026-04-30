ffc463f366a34c07006b2f56ccc92da4dc1c4c34
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-04-30 16:33:53 -0700

    Fix merge conflicts

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,8 @@
 declare namespace _default {
-<<<<<<< Updated upstream
-    export { fetcher };
-    export { errors };
-}
-export default _default;
-import * as fetcher from "./fetcher";
-import * as errors from "./publicErrors";
-export { fetcher, errors };
-=======
   export { fetcher };
-  export { parser };
   export { errors };
 }
 export default _default;
 import * as fetcher from "./fetcher.js";
-import * as parser from "./parser.js";
 import * as errors from "./publicErrors.js";
-export { fetcher, parser, errors };
->>>>>>> Stashed changes
+export { fetcher, errors };
--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-<<<<<<< Updated upstream
-import * as fetcher from "./fetcher";
-import * as errors from "./publicErrors";
-=======
 import * as fetcher from "./fetcher.js";
-import * as parser from "./parser.js";
 import * as errors from "./publicErrors.js";
->>>>>>> Stashed changes
 
 export { fetcher, errors };
 export default { fetcher, errors };
